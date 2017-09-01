0c7216e01aee82a6b5e74427d3aa80ba2734ac10
huangxiong <huangxionging@qq.com>
2017-09-01 16:42:38 +0800

    更新

---
+++ Initializers/Initializers/UntitiledDocument.swift
@@ -0,0 +1,14 @@
+//
+//  UntitiledDocument.swift
+//  Initializers
+//
+//  Created by huangxiong on 2017/9/1.
+//  Copyright © 2017年 huangxiong. All rights reserved.
+//
+
+import Foundation
+class UntitiledDocument: Document {
+    override init() {
+        super.init(name: "[Untitled]")!
+    }
+}
